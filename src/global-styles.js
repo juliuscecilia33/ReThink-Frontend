03a710ec47ac8da00fa24b30d9f03715d918457c
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-01-26 21:35:39 -0800

    global styles

---
+++ src/global-styles.js
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    html {
+        -webkit-box-sizing: border-box;
+                box-sizing: border-box;
+        scroll-behavior: smooth;
+    }
+
+    *, *:before, *:after {
+        -webkit-box-sizing: inherit;
+        box-sizing: inherit;
+    }
+
+    body {
+        margin: 0;
+        padding: 0;
+        line-height: 1;
+        font-family: 'Roboto', sans-serif;
+        color: #000000;
+        background-color: #FFFFFC;
+        font-smooth: always;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+
+    ul {
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+        list-style: none;
+    }
+
+    h1, h2, h3, p {
+        margin: 0;
+        padding: 0;
+    }
+
+    a {
+        border: none;
+        background: none;
+        outline: none;
+        margin: 0;
+        padding: 0;
+        text-decoration: none;
+        outline: none;
+        color: #fff;
+        cursor: pointer;
+    }
+
+    button {
+        margin: 0;
+        padding: 0;
+    }
+`;

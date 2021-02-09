f5d4deeb472d6a23bf755749c1e9e1886ec7b101
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-02-08 17:22:20 -0800

    template

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from "react";
-import * as ROUTES from "./constants/routes";
-import { NavbarContainer } from "./containers";
+import * as ROUTES from "./constants/routes"; // you can add routes here!
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <Router>
-      <NavbarContainer />
-
       <Switch>
         <Route path={ROUTES.HOME} exact>
           <div>Home Page</div>
         </Route>
 
-        <Route path={ROUTES.ABOUT} exact>
-          <div>About Page</div>
-        </Route>
-
-        <Route path={ROUTES.BUSINESSES} exact>
-          <div>Business Page</div>
-        </Route>
-
-        <Route path={ROUTES.PROMOTE} exact>
-          <div>Promote Page</div>
-        </Route>
-
-        <Route path={ROUTES.CONTACT} exact>
-          <div>Contact Page</div>
-        </Route>
+        {/* Add routes here! */}
       </Switch>
     </Router>
   );
